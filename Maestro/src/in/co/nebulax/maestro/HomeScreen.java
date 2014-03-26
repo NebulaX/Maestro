1e787e17159c1d8b45274d78b4d5b26781b71bd9
Akshay Deshraj <akshay30nov@gmail.com>
2014-03-26 21:19:38 +0530

    Prelimnary code for login

--- Maestro/src/in/co/nebulax/maestro/HomeScreen.java
+++ Maestro/src/in/co/nebulax/maestro/HomeScreen.java
@@ -1,14 +1,22 @@
 package in.co.nebulax.maestro;
 
+import com.parse.ParseUser;
+
 import android.os.Bundle;
+import android.widget.TextView;
 
-import com.actionbarsherlock.app.SherlockActivity;
+public class HomeScreen extends MyActivity {
 
-public class HomeScreen extends SherlockActivity {
-
+	TextView usrName;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		usrName = (TextView) findViewById(R.id.tv_usrName);
+		
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		
+		usrName.setText(currentUser.getUsername());
 	}
 }
--- Maestro/src/in/co/nebulax/maestro/LoginActivity.java
+++ Maestro/src/in/co/nebulax/maestro/LoginActivity.java
@@ -1,11 +1,17 @@
 package in.co.nebulax.maestro;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends MyActivity {
 
@@ -13,6 +19,7 @@ public class LoginActivity extends MyActivity {
 	EditText usrPass;
 	Button btnLogin;
 	TextView reg_redirect;
+	boolean isLoginSuccess = true;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +51,7 @@ public class LoginActivity extends MyActivity {
 		switch(v.getId()){
 		case R.id.btn_login:
 			//Add code to send request for login
+			new LoginHandler().execute();
 			break;
 		case R.id.tv_reg_redirect :
 			finish();
@@ -53,4 +61,48 @@ public class LoginActivity extends MyActivity {
 		}
 
 	}
+	
+	// Async Task class for making HTTP call
+		private class LoginHandler extends AsyncTask<Integer, Void, Void> {
+
+			@Override
+			protected void onPreExecute() {
+				super.onPreExecute();
+				// Showing progress dialog
+				pDialog = new ProgressDialog(LoginActivity.this);
+				pDialog.setMessage("Please wait...");
+				pDialog.setCancelable(false);
+				pDialog.show();
+			}
+
+			@Override
+			protected Void doInBackground(Integer... arg0) {
+				
+				try {
+					ParseUser.logIn(usrName.getText().toString(), 
+							usrPass.getText().toString());
+				} catch (ParseException e) {
+					e.printStackTrace();
+					isLoginSuccess = false;
+					Toast.makeText(LoginActivity.this, "Something Wicked Happened !!",
+							Toast.LENGTH_SHORT).show();
+				}
+				
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(Void result) {
+				super.onPostExecute(result);
+				// Dismiss the progress dialog
+				if (pDialog.isShowing())
+					pDialog.dismiss();
+				if(isLoginSuccess){
+					finish();
+					Intent intent = new Intent(LoginActivity.this , HomeScreen.class);
+					startActivity(intent);
+				}
+			}
+			
+		}
 }
