6064f9518dd74ec4fbe00b9b5de4fb743b47e3b2
Akshay Deshraj <akshay30nov@gmail.com>
2014-03-29 15:18:45 +0530

    Add to do for cloud code

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -4,3 +4,8 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+// Status code 0 : request made ; 1 : process started ; 2 : process completed successfully
+// Function to set status code to 1 and assign a tutor ; return no tutor availble in ur locality in case of no tutor
+// After tutor is assigned , he must be sent a push notification consisting of name of student , subject and topic along with his phone number
+// If the tutor accepts the request , a notification must be sent to the student with details of the tutor
