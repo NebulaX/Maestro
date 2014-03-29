aca8f9680c3826eed237d88eaa2d9776a0bf19be
Akshay Deshraj <akshay30nov@gmail.com>
2014-03-29 15:07:19 +0530

    Add code to request maestro

--- Maestro/src/in/co/nebulax/maestro/PortalStudent.java
+++ Maestro/src/in/co/nebulax/maestro/PortalStudent.java
@@ -1,16 +1,46 @@
 package in.co.nebulax.maestro;
 
+import in.co.nebulax.maestro.utils.ConnectionDetector;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.ActionBar.TabListener;
 import com.actionbarsherlock.app.SherlockFragment;
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
-public class PortalStudent extends SherlockFragment implements TabListener{
+public class PortalStudent extends SherlockFragment implements TabListener,
+		OnItemSelectedListener {
+
+	View v;
+
+	ParseUser currUser;
+	TextView tv_usrName;
+	Button btn_requestTutor, submit;
+	LinearLayout requestBuilder;
+	Spinner subject_select;
+
+	EditText topic_name;
+
+	ProgressDialog pDialog;
+	public ConnectionDetector cd;
+	public Boolean isInternetPresent;
+
+	String subject = "English";
 
 	public PortalStudent() {
 	}
@@ -18,16 +48,79 @@ public class PortalStudent extends SherlockFragment implements TabListener{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
-		View v = inflater.inflate(R.layout.fragment_student, container , false);
-		
+
+		v = inflater.inflate(R.layout.fragment_student, container, false);
+
+		Parse.initialize(getSherlockActivity(),
+				getResources().getString(R.string.APP_ID), getResources()
+						.getString(R.string.CLIENT_KEY));
+
+		initialiseFields();
+		setFields();
 		return v;
+	}
+
+	public void initialiseFields() {
+
+		// Initialising fields
+		tv_usrName = (TextView) v.findViewById(R.id.fragStudent_tv_name);
+		btn_requestTutor = (Button) v
+				.findViewById(R.id.fragStudent_btn_tutRequest);
+
+		requestBuilder = (LinearLayout) v
+				.findViewById(R.id.fragStudent_ll_tut_request);
+
+		subject_select = (Spinner) v
+				.findViewById(R.id.fragStudent_spinner_subjectSelect);
+
+		currUser = ParseUser.getCurrentUser();
+		tv_usrName.setText("Hi " + currUser.getString("Name"));
+
+		topic_name = (EditText) v.findViewById(R.id.fragStudent_topic_name);
+		submit = (Button) v.findViewById(R.id.fragStudent_btn_submit);
+	}
+
+	public void setFields() {
+
+		// set fields
+		btn_requestTutor.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				requestBuilder.setVisibility(View.VISIBLE);
+			}
+		});
+
+		subject_select.setOnItemSelectedListener(this);
+		
+		submit.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (isValidated()) {
+
+					ParseObject request = new ParseObject("RequestLog");
+					request.put("StudentId", currUser.getObjectId());
+					request.put("Subject", subject);
+					request.put("Topic", topic_name.getText().toString());
+					request.put("Status",0) ;
+					request.saveEventually();
+					Toast.makeText(getSherlockActivity(), "Request Recieved",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+	}
+
+	public boolean isValidated() {
+
+		return true;
 	}
 
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
-		ft.add(android.R.id.content, this,"Student");
-        ft.attach(this);
+		ft.add(android.R.id.content, this, "Student");
+		ft.attach(this);
 	}
 
 	@Override
@@ -37,9 +130,20 @@ public class PortalStudent extends SherlockFragment implements TabListener{
 
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
-		
+
 	}
-	
-	
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View v, int pos, long arg3) {
+
+		subject = parent.getItemAtPosition(pos).toString();
+
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
--- Maestro/src/in/co/nebulax/maestro/utils/Subjects.java
+++ Maestro/src/in/co/nebulax/maestro/utils/Subjects.java
@@ -1,51 +1,51 @@
-package in.co.nebulax.maestro.utils;
-
-public class Subjects {
-
-	public static String[][] subjects = new String[][] {
-
-			// Class 1
-			{ "English", "Hindi", "Maths", "E.V.S" },
-
-			// Class 2
-			{ "English", "Hindi", "Maths", "E.V.S" },
-
-			// Class 3
-			{ "English", "Hindi", "Maths", "E.V.S", "Social Studies" },
-
-			// Class 4
-			{ "English", "Hindi", "Maths", "Science", "Social Studies" },
-
-			// Class 5
-			{ "English", "Hindi", "Maths", "Science", "Social Studies" },
-
-			// Class 6
-			{ "English", "Hindi", "Maths", "Science", "Social Studies",
-					"Sanskrit" },
-
-			// Class 7
-			{ "English", "Hindi", "Maths", "Science", "Social Studies",
-					"Sanskrit" },
-
-			// Class 8
-			{ "English", "Hindi", "Maths", "Science", "Social Studies",
-					"Sanskrit" },
-
-			// Class 9
-			{ "English", "Hindi", "Maths", "Science", "Social Studies",
-					"Sanskrit" },
-
-			// Class 10
-			{ "English", "Hindi", "Maths", "Science", "Social Studies",
-					"Sanskrit" },
-
-			// Class 11
-			{ "English", "Hindi", "Maths", "Physics", "Chemistry",
-					"Accountancy", "Business Studies", "Political Science",
-					"History" },
-
-			// Class 12
-			{ "English", "Hindi", "Maths", "Physics", "Chemistry",
-					"Accountancy", "Business Studies", "Political Science",
-					"History" }, };
-}
+//package in.co.nebulax.maestro.utils;
+//
+//public class Subjects {
+//
+//	public static String[][] subjects = new String[][] {
+//
+//			// Class 1
+//			{ "English", "Hindi", "Maths", "E.V.S" },
+//
+//			// Class 2
+//			{ "English", "Hindi", "Maths", "E.V.S" },
+//
+//			// Class 3
+//			{ "English", "Hindi", "Maths", "E.V.S", "Social Studies" },
+//
+//			// Class 4
+//			{ "English", "Hindi", "Maths", "Science", "Social Studies" },
+//
+//			// Class 5
+//			{ "English", "Hindi", "Maths", "Science", "Social Studies" },
+//
+//			// Class 6
+//			{ "English", "Hindi", "Maths", "Science", "Social Studies",
+//					"Sanskrit" },
+//
+//			// Class 7
+//			{ "English", "Hindi", "Maths", "Science", "Social Studies",
+//					"Sanskrit" },
+//
+//			// Class 8
+//			{ "English", "Hindi", "Maths", "Science", "Social Studies",
+//					"Sanskrit" },
+//
+//			// Class 9
+//			{ "English", "Hindi", "Maths", "Science", "Social Studies",
+//					"Sanskrit" },
+//
+//			// Class 10
+//			{ "English", "Hindi", "Maths", "Science", "Social Studies",
+//					"Sanskrit" },
+//
+//			// Class 11
+//			{ "English", "Hindi", "Maths", "Physics", "Chemistry",
+//					"Accountancy", "Business Studies", "Political Science",
+//					"History" },
+//
+//			// Class 12
+//			{ "English", "Hindi", "Maths", "Physics", "Chemistry",
+//					"Accountancy", "Business Studies", "Political Science",
+//					"History" }, };
+//}
